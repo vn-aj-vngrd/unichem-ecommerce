8c2784651baea17a224a4738709ef2923905ad7c
RG-JR <93367124+RG-JR@users.noreply.github.com>
2022-02-21 15:16:02 +0800

    Update Wishlist.js

--- src/pages/Store/Wishlist.js
+++ src/pages/Store/Wishlist.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Wishlist = () => {
   return (
     // margin-top 200 so that container may be visible
-    <div className="container mt-200">Wishlist OIDUHFOIDVHDVBLIVHSUIVH</div>
+    <div className="container mt-200">Wishlist TEST 2</div>
   )
 }
 
