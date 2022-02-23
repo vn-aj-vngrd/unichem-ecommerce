18e724680469953ef294791413bb77441ecdaefd
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-02-23 20:18:19 +0800

    commit

---
+++ backend/models/addressModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const addressSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    address1: {
+      type: String,
+      required: [true, "Please add your Region, Province, City, Barangay."],
+    },
+    address2: {
+      type: String,
+      required: [true, "Please add your Street Name, Building, House No."],
+      unique: true,
+    },
+    postalCode: {
+      type: String,
+      required: [true, "Please add your Postal Code."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Address", addressSchema);
