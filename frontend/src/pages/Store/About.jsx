d4cea86e235d2a18b42fad0bf3efafb125c1fa17
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-02-26 15:10:16 +0800

    commit

--- frontend/src/pages/Store/About.jsx
+++ frontend/src/pages/Store/About.jsx
@@ -7,19 +7,19 @@ const About = () => {
 
   return (
     <div className="body-content">
-      <section class="about-us">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6 col-md-12 col-12">
-              <div class="content-left">
+      <section className="about-us">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-12 col-12">
+              <div className="content-left">
                 <img
                   src="https://images.unsplash.com/photo-1549194388-2469d59ec75c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
                   alt="#"
                 />
               </div>
             </div>
-            <div class="col-lg-6 col-md-12 col-12">
-              <div class="content-right">
+            <div className="col-lg-6 col-md-12 col-12">
+              <div className="content-right">
                 <h2>Unichem Industrial Sales Inc.</h2>
                 <p>
                   UNICHEM INDUSTRIAL SALES, INC. is located in Mandaue City,
@@ -44,46 +44,46 @@ const About = () => {
         </div>
       </section>
 
-      <section class="team section">
-        <div class="container">
-          <div class="row">
-            <div class="col-12">
-              <div class="section-title">
-                <h2 class="wow fadeInUp" data-wow-delay=".4s">
+      <section className="team section">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="section-title">
+                <h2 className="wow fadeInUp" data-wow-delay=".4s">
                   Our Developer Team
                 </h2>
-                <p class="wow fadeInUp" data-wow-delay=".6s">
+                <p className="wow fadeInUp" data-wow-delay=".6s">
                   Meet the developers of Unichem E-store.
                 </p>
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-lg-3 col-md-6 col-12">
-              <div class="single-team">
-                <div class="image">
+          <div className="row">
+            <div className="col-lg-3 col-md-6 col-12">
+              <div className="single-team">
+                <div className="image">
                   <img
                     src="https://scontent.fceb2-2.fna.fbcdn.net/v/t39.30808-6/265045880_4664465363610412_3077703784126999012_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeG6OCYCZ3CHvDCnn3gkw4f07VEmKdmD4HrtUSYp2YPgeijaAgkLlVvIZi6OkafPGr9I9Ew1JO3LnF6iAYvJVZH-&_nc_ohc=aqCKcJTr730AX_jh2lf&_nc_ht=scontent.fceb2-2.fna&oh=00_AT_6BqovlMtCUeTqci4MFGHSItKRq0HLzJiVupFteSUTcA&oe=621DED01"
                     alt="#"
                   />
                 </div>
-                <div class="content">
-                  <div class="info">
+                <div className="content">
+                  <div className="info">
                     <h3>Barigga, Horeb</h3>
-                    <ul class="social">
+                    <ul className="social">
                       <li>
                         <a href="/">
-                          <i class="lni lni-facebook-filled"></i>
+                          <i className="lni lni-facebook-filled"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-github-original"></i>
+                          <i className="lni lni-github-original"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-instagram-filled"></i>
+                          <i className="lni lni-instagram-filled"></i>
                         </a>
                       </li>
                     </ul>
@@ -91,31 +91,31 @@ const About = () => {
                 </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-12">
-              <div class="single-team">
-                <div class="image">
+            <div className="col-lg-3 col-md-6 col-12">
+              <div className="single-team">
+                <div className="image">
                   <img
                     src="https://scontent.fceb2-2.fna.fbcdn.net/v/t1.6435-9/60255777_10205808349163781_6229416713532735488_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGuXE5hs2F1PtWOpccTSnbAY9sEjR8tFldj2wSNHy0WVwxTPmfOm9dMbU0rZS91gaS6ruU5b0c9R-0Mb9BnOSGx&_nc_ohc=uyKs1itBYK8AX9T4ObU&_nc_ht=scontent.fceb2-2.fna&oh=00_AT_nN_XimotmMXGDCcMPgNYFxf3aTpUZ-LbCoT619FIXIQ&oe=623E115D"
                     alt="#"
                   />
                 </div>
-                <div class="content">
-                  <div class="info">
+                <div className="content">
+                  <div className="info">
                     <h3>Gelacio, Roque</h3>
-                    <ul class="social">
+                    <ul className="social">
                       <li>
                         <a href="/">
-                          <i class="lni lni-facebook-filled"></i>
+                          <i className="lni lni-facebook-filled"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-github-original"></i>
+                          <i className="lni lni-github-original"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-instagram-filled"></i>
+                          <i className="lni lni-instagram-filled"></i>
                         </a>
                       </li>
                     </ul>
@@ -123,31 +123,31 @@ const About = () => {
                 </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-12">
-              <div class="single-team">
-                <div class="image">
+            <div className="col-lg-3 col-md-6 col-12">
+              <div className="single-team">
+                <div className="image">
                   <img
                     src="https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/271657462_4899150540105361_3610241298101667884_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFlBi2QrWTsC6h8G6x_yldUISWEXve9bfohJYRe971t-nY_yoEBIbmpAfl8Fl9bN4QBHO6GTFtJ9-adM6MoQwQR&_nc_ohc=ufndT4srzboAX80ILUL&_nc_ht=scontent.fceb2-1.fna&oh=00_AT-A8grgTFsZSuLZuD7U947C3px4AWiUgjERlAcuTAxZKQ&oe=621E0BA2"
                     alt="#"
                   />
                 </div>
-                <div class="content">
-                  <div class="info">
+                <div className="content">
+                  <div className="info">
                     <h3>Tumapon, Nikolai Franz</h3>
-                    <ul class="social">
+                    <ul className="social">
                       <li>
                         <a href="/">
-                          <i class="lni lni-facebook-filled"></i>
+                          <i className="lni lni-facebook-filled"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-github-original"></i>
+                          <i className="lni lni-github-original"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-instagram-filled"></i>
+                          <i className="lni lni-instagram-filled"></i>
                         </a>
                       </li>
                     </ul>
@@ -155,31 +155,31 @@ const About = () => {
                 </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-12">
-              <div class="single-team">
-                <div class="image">
+            <div className="col-lg-3 col-md-6 col-12">
+              <div className="single-team">
+                <div className="image">
                   <img
                     src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/89595114_2790668074362384_5253522795815501824_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=a4a2d7&_nc_eui2=AeHXsn03IiqEn6omaUEIHM_Bi_8pd_rKJnGL_yl3-somcSQcauFysieA3LXgKI8yhRkZ1o5VDJ3MtPnrn4tZ3Kzw&_nc_ohc=GUU2jNVQE6sAX-Ni2kI&tn=k5PFbSYe6hUkAfvk&_nc_ht=scontent.fcrk4-1.fna&oh=00_AT-LMPLrqajMuCM7JZGz86dSo4VMp6jG82Tc1BzBiuluWA&oe=623E037F"
                     alt="#"
                   />
                 </div>
-                <div class="content">
-                  <div class="info">
+                <div className="content">
+                  <div className="info">
                     <h3>Vanguardia, Van AJ</h3>
-                    <ul class="social">
+                    <ul className="social">
                       <li>
                         <a href="/">
-                          <i class="lni lni-facebook-filled"></i>
+                          <i className="lni lni-facebook-filled"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-github-original"></i>
+                          <i className="lni lni-github-original"></i>
                         </a>
                       </li>
                       <li>
                         <a href="/">
-                          <i class="lni lni-instagram-filled"></i>
+                          <i className="lni lni-instagram-filled"></i>
                         </a>
                       </li>
                     </ul>
--- frontend/src/pages/Store/Cart.jsx
+++ frontend/src/pages/Store/Cart.jsx
@@ -6,30 +6,30 @@ const Cart = () => {
   });
 
   return (
-    <div class="shopping-cart body-content">
-      <div class="container">
-        <div class="cart-list-head">
-          <div class="cart-list-title">
-            <div class="row">
-              <div class="col-lg-1 col-md-1 col-12"></div>
-              <div class="col-lg-4 col-md-3 col-12">
+    <div className="shopping-cart body-content">
+      <div className="container">
+        <div className="cart-list-head">
+          <div className="cart-list-title">
+            <div className="row">
+              <div className="col-lg-1 col-md-1 col-12"></div>
+              <div className="col-lg-4 col-md-3 col-12">
                 <p>Product Name</p>
               </div>
-              <div class="col-lg-2 col-md-2 col-12">
+              <div className="col-lg-2 col-md-2 col-12">
                 <p>Quantity</p>
               </div>
-              <div class="col-lg-2 col-md-2 col-12">
+              <div className="col-lg-2 col-md-2 col-12">
                 <p>Subtotal</p>
               </div>
-              <div class="col-lg-1 col-md-2 col-12">
+              <div className="col-lg-1 col-md-2 col-12">
                 <p>Remove</p>
               </div>
             </div>
           </div>
 
-          <div class="cart-single-list">
-            <div class="row align-items-center">
-              <div class="col-lg-1 col-md-1 col-12">
+          <div className="cart-single-list">
+            <div className="row align-items-center">
+              <div className="col-lg-1 col-md-1 col-12">
                 <a href="product-details.html">
                   <img
                     src="https://dm.henkel-dam.com/is/image/henkel/Loctite_271_232077_APAC_50ml_2019_12.15_QRcode?wid=2048&fit=fit%2C1&qlt=90&align=0%2C0&hei=2048"
@@ -37,19 +37,19 @@ const Cart = () => {
                   />
                 </a>
               </div>
-              <div class="col-lg-4 col-md-3 col-12">
-                <h5 class="product-name">
+              <div className="col-lg-4 col-md-3 col-12">
+                <h5 className="product-name">
                   <a href="product-details.html">Test</a>
                 </h5>
-                <p class="product-des">
+                <p className="product-des">
                   <span>
                     <b>Type / Color:</b> test
                   </span>
                 </p>
               </div>
-              <div class="col-lg-2 col-md-2 col-12">
-                <div class="count-input">
-                  <select class="form-control">
+              <div className="col-lg-2 col-md-2 col-12">
+                <div className="count-input">
+                  <select className="form-control">
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -58,27 +58,27 @@ const Cart = () => {
                   </select>
                 </div>
               </div>
-              <div class="col-lg-2 col-md-2 col-12">
+              <div className="col-lg-2 col-md-2 col-12">
                 <p>$910.00</p>
               </div>
-              <div class="col-lg-2 col-md-2 col-12">
+              <div className="col-lg-2 col-md-2 col-12">
                 <p>$29.00</p>
               </div>
-              <div class="col-lg-1 col-md-2 col-12">
-                <a class="remove-item" href="/">
-                  <i class="lni lni-close"></i>
+              <div className="col-lg-1 col-md-2 col-12">
+                <a className="remove-item" href="/">
+                  <i className="lni lni-close"></i>
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-12">
-            <div class="total-amount">
-              <div class="row">
-                <div class="col-lg-8 col-md-6 col-12"></div>
-                <div class="col-lg-4 col-md-6 col-12">
-                  <div class="right">
+        <div className="row">
+          <div className="col-12">
+            <div className="total-amount">
+              <div className="row">
+                <div className="col-lg-8 col-md-6 col-12"></div>
+                <div className="col-lg-4 col-md-6 col-12">
+                  <div className="right">
                     <ul>
                       <li>
                         Cart Subtotal<span>$2560.00</span>
@@ -86,15 +86,15 @@ const Cart = () => {
                       <li>
                         Shipping Fee<span>Free</span>
                       </li>
-                      <li class="last">
+                      <li className="last">
                         Total Payment<span>$2531.00</span>
                       </li>
                     </ul>
-                    <div class="button">
-                      <a href="checkout.html" class="btn">
+                    <div className="button">
+                      <a href="checkout.html" className="btn">
                         Checkout
                       </a>
-                      <a href="product-grids.html" class="btn btn-alt">
+                      <a href="product-grids.html" className="btn btn-alt">
                         Continue shopping
                       </a>
                     </div>
