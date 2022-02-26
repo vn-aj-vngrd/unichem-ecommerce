4d67800a56e752391c84befc5fda310d1fec6335
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-02-26 15:14:00 +0800

    commit

--- frontend/src/pages/Store/Contact.jsx
+++ frontend/src/pages/Store/Contact.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   useEffect(() => {
-    document.title = "Unichem | Products";
+    document.title = "Unichem | Contact";
   });
 
   return (
