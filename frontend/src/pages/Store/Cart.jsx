037c1f16610369fbb61424f7b5ee69f6902785bd
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-02-26 15:23:26 +0800

    commit

--- frontend/src/pages/Store/Cart.jsx
+++ frontend/src/pages/Store/Cart.jsx
@@ -1,6 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
+  const [counter, setCounter] = useState(1);
+  let decrement, increment;
+  if (counter > 1) decrement = () => setCounter(counter - 1);
+  if (counter < 6) increment = () => setCounter(counter + 1);
+
   useEffect(() => {
     document.title = "Unichem | Cart";
   });
@@ -19,7 +24,10 @@ const Cart = () => {
                 <p>Quantity</p>
               </div>
               <div className="col-lg-2 col-md-2 col-12">
-                <p>Subtotal</p>
+                <p>Price</p>
+              </div>
+              <div className="col-lg-2 col-md-2 col-12">
+                <p>Total</p>
               </div>
               <div className="col-lg-1 col-md-2 col-12">
                 <p>Remove</p>
@@ -48,21 +56,42 @@ const Cart = () => {
                 </p>
               </div>
               <div className="col-lg-2 col-md-2 col-12">
-                <div className="count-input">
-                  <select className="form-control">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </select>
+                <div className="pe-2 bd-highlight">
+                  <div className="form-group quantity">
+                    <div className="quantity-control">
+                      <button className="quantity-btn" onClick={decrement}>
+                        <svg viewBox="0 0 409.6 409.6">
+                          <g>
+                            <g>
+                              <path d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467 c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z" />
+                            </g>
+                          </g>
+                        </svg>
+                      </button>
+                      <input
+                        type="number"
+                        className="quantity-input"
+                        value={counter}
+                        step="0.1"
+                        min="1"
+                        max=""
+                        name="quantity"
+                        readOnly
+                      />
+                      <button className="quantity-btn" onClick={increment}>
+                        <svg viewBox="0 0 426.66667 426.66667">
+                          <path d="m405.332031 192h-170.664062v-170.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031-11.773437 0-21.332031 9.558594-21.332031 21.332031v170.667969h-170.667969c-11.773437 0-21.332031 9.558594-21.332031 21.332031 0 11.777344 9.558594 21.335938 21.332031 21.335938h170.667969v170.664062c0 11.777344 9.558594 21.335938 21.332031 21.335938 11.777344 0 21.335938-9.558594 21.335938-21.335938v-170.664062h170.664062c11.777344 0 21.335938-9.558594 21.335938-21.335938 0-11.773437-9.558594-21.332031-21.335938-21.332031zm0 0" />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="col-lg-2 col-md-2 col-12">
-                <p>$910.00</p>
+                <p>₱910.00</p>
               </div>
               <div className="col-lg-2 col-md-2 col-12">
-                <p>$29.00</p>
+                <p>₱2000.00</p>
               </div>
               <div className="col-lg-1 col-md-2 col-12">
                 <a className="remove-item" href="/">
@@ -81,13 +110,13 @@ const Cart = () => {
                   <div className="right">
                     <ul>
                       <li>
-                        Cart Subtotal<span>$2560.00</span>
+                        Cart Subtotal<span>₱2000.00</span>
                       </li>
                       <li>
                         Shipping Fee<span>Free</span>
                       </li>
                       <li className="last">
-                        Total Payment<span>$2531.00</span>
+                        Total Payment<span>₱2000.00</span>
                       </li>
                     </ul>
                     <div className="button">
--- frontend/src/pages/Store/Contact.jsx
+++ frontend/src/pages/Store/Contact.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 
 const Login = () => {
   useEffect(() => {
