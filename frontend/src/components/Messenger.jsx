410592baa9fc45724632553deaa6e31af6bd354d
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-02-27 16:18:08 +0800

    commit

--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -3,7 +3,6 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 const Messenger = () => {
   return (
     <div>
-      {" "}
       <MessengerCustomerChat
         pageId="101323719167913"
         appId="3284833775173886"
