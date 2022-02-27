0a69aa93391e803fce1665bc7098084bfbb15bdb
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-02-27 15:35:28 +0800

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
     process.exit(1);
   }
 };
