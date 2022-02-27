eea4f05efc7f9df355cca90ea822f28d1c5bdfee
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-02-27 15:37:56 +0800

    commit

--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -1,17 +1,17 @@
-import MessengerCustomerChat from "react-messenger-customer-chat";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
 
-const Messenger = () => {
-  return (
-    <div>
-      {" "}
-      <MessengerCustomerChat
-        pageId="101323719167913"
-        appId="3284833775173886"
-        // htmlRef="<REF_STRING>"
-      />
-      ,
-    </div>
-  );
-};
+// const Messenger = () => {
+//   return (
+//     <div>
+//       {" "}
+//       <MessengerCustomerChat
+//         pageId="101323719167913"
+//         appId="3284833775173886"
+//         // htmlRef="<REF_STRING>"
+//       />
+//       ,
+//     </div>
+//   );
+// };
 
-export default Messenger;
+// export default Messenger;
