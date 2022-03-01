9c09681f03319e933a011f59ea96513e588062c3
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-01 18:57:31 +0800

    commit

--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -7,9 +7,12 @@ const Messenger = () => {
         pageId="101323719167913"
         appId="3284833775173886"
         themeColor="#f44336"
+        loggedInGreeting="Welcome to Unichem support service, we are here to assist you"
+        loggedOutGreeting="Thank you for your interest in Unichem, we are glad to assist you."
+        greetingDialogDisplay="fade"
+        greetingDialogDelay="5000"
         // htmlRef="<REF_STRING>"
       />
-      ,
     </div>
   );
 };
