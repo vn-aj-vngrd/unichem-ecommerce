a1520b2f459380aff7b429679c2ded482591e7e8
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-01 18:43:01 +0800

    commit

--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -6,7 +6,7 @@ const Messenger = () => {
       <MessengerCustomerChat
         pageId="101323719167913"
         appId="3284833775173886"
-        themeColor="f44336"
+        themeColor="#f44336"
         // htmlRef="<REF_STRING>"
       />
       ,
