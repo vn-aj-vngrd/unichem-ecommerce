fb25b8d285d8872bfb6583ebd5c20c9b0374b09f
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-04 23:40:20 +0800

    commit

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,13 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
-const cart = require("../models/cartModel");
+const Cart = require("../models/cartModel");
 
 // @desc    Get all carts
 // @route   GET /api/carts
 // @access  Private
 const getCartItems = asyncHandler(async (req, res) => {
-  // const carts = await cart.find();
-  // return res.status(200).json(carts);
+  const carts = await Cart.find();
+  return res.status(200).json(carts);
 });
 
 // @desc    Set cart
@@ -23,11 +23,11 @@ const updateCartItem = asyncHandler(async (req, res) => {});
 // @desc    Delete cart
 // @route   DELETE /api/carts/:id
 // @access  Private
-const deleteCartItems = asyncHandler(async (req, res) => {});
+const deleteCartItem = asyncHandler(async (req, res) => {});
 
 module.exports = {
   getCartItems,
   setCartItem,
   updateCartItem,
-  deleteCartItems,
+  deleteCartItem,
 };
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,6 @@ const Product = require("../models/productModel");
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find();
-
   return res.status(200).json(products);
 });
 
@@ -21,7 +20,6 @@ const setProduct = asyncHandler(async (req, res) => {
   //     res.status(400);
   //     throw new Error('Please enter a valid input for all fields.');
   //   }
-
   //   const product = await Product.create({
   //     productName: req.body.productName,
   //     brand: req.body.brand,
@@ -34,23 +32,18 @@ const setProduct = asyncHandler(async (req, res) => {
   //     quantities: req.body.quantities,
   //     price: req.body.price,
   //   })
-
   //   res.status(200).json(product);
 });
 
 // @desc    Update Product
 // @route   PUT /api/products/:id
 // @access  Private
-const updateProduct = asyncHandler(async (req, res) => {
-
-});
+const updateProduct = asyncHandler(async (req, res) => {});
 
 // @desc    Delete Product
 // @route   DELETE /api/products/:id
 // @access  Private
-const deleteProduct = asyncHandler(async (req, res) => {
-
-});
+const deleteProduct = asyncHandler(async (req, res) => {});
 
 module.exports = {
   getProducts,
