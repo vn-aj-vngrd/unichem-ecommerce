72c18a6dc01876a7b699ac4b44a3ffa8a882b3ca
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-11 17:47:36 +0800

    commit

--- frontend/src/pages/Product/ProductDetails.jsx
+++ frontend/src/pages/Product/ProductDetails.jsx
@@ -6,20 +6,20 @@ import Reviews from "../../components/Reviews";
 import Breadcrumb from "../../components/Breadcrumb";
 
 const ProductDetails = () => {
-  const location = useLocation();
+
   useEffect(() => {
     document.title = "Unichem Store | Product Details";
-  }, [location.key]);
+  });
 
   let { id } = useParams();
   console.log(id);
 
   return (
     <>
-      {/* <Breadcrumb type="product"/> 
-      <Details product={product} />
-      <Specifications product={product} />
-      <Reviews /> */}
+      <Breadcrumb type="product" />
+      {/* <Details product={} />
+      <Specifications product={} /> */}
+      <Reviews />
     </>
   );
 };
