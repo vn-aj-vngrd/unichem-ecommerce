8ba5d28cb3d029314a5fba366b3dad2cd6f65ecb
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-28 19:40:58 +0800

    Update Wishlist.jsx

--- frontend/src/pages/Store/Wishlist.jsx
+++ frontend/src/pages/Store/Wishlist.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Breadcrumb from "../../components/Breadcrumb";
