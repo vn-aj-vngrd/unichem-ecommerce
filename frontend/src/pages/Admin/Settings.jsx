ce6a8de674e8d2026bf36fa95e7d55b85178a1e5
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-29 12:33:13 +0800

    commit

--- frontend/src/pages/Admin/Settings.jsx
+++ frontend/src/pages/Admin/Settings.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import SectionTitle from "../../components/SectionTitle";
+import logo from "../../assets/images/logo.svg";
 
 const Home = () => {
   useEffect(() => {
@@ -12,27 +13,27 @@ const Home = () => {
       <Header />
       <SectionTitle type="settings" />
       <div className="row mt-3">
-        <div class="col-12 col-xl-8">
-          <div class="card card-body border-0 shadow mb-4">
-            <h2 class="h5 mb-4">Admin Information</h2>
+        <div className="col-12 col-xl-8">
+          <div className="card card-body border-0 shadow mb-4">
+            <h2 className="h5 mb-4">Admin Information</h2>
             <form>
-              <div class="row">
-                <div class="col-md-6 mb-3">
+              <div className="row">
+                <div className="col-md-6 mb-3">
                   <div>
                     <label>Email</label>
                     <input
-                      class="form-control"
+                      className="form-control"
                       type="email"
                       value="Sample Text..."
                       required
                     />
                   </div>
                 </div>
-                <div class="col-md-6 mb-3">
+                <div className="col-md-6 mb-3">
                   <div>
                     <label>Name</label>
                     <input
-                      class="form-control"
+                      className="form-control"
                       type="text"
                       value="Sample Text..."
                       required
@@ -40,12 +41,12 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-              <div class="row">
-                <div class="col-md-6 mb-3">
+              <div className="row">
+                <div className="col-md-6 mb-3">
                   <div>
                     <label>Password</label>
                     <input
-                      class="form-control"
+                      className="form-control"
                       type="password"
                       value="Sample Text..."
                       required
@@ -59,14 +60,27 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div class="col-12 col-xl-4">
-          <div class="row">
-            <div class="col-12 mb-4">
-              <div class="card shadow border-0 text-center p-0">
-                <div class="card-body pb-5 pt-5">
-                  <h4 class="h3">Administrator</h4>
-                  <h5 class="fw-normal">Unichem Store</h5>
-                  <p class="text-gray mb-4">Mandaue City, Cebu PH</p>
+        <div className="col-12 col-xl-4">
+          <div className="row">
+            <div className="col-12 mb-4">
+              <div
+                className="card shadow border-0 text-center p-0"
+                style={{ height: `326px` }}
+              >
+                <div
+                  className="container p-3"
+                  style={{ backgroundColor: `#424242` }}
+                >
+                  <img
+                    src={logo}
+                    className="avatar-xl rounded-circle mx-auto"
+                    alt="Admin"
+                  />
+                </div>
+                <div className="card-body pb-3 pt-4">
+                  <h4 className="h3">Administrator</h4>
+                  <h5 className="fw-normal">Unichem Store</h5>
+                  <p className="text-gray">Mandaue City, Cebu PH</p>
                 </div>
               </div>
             </div>
