37b58a21d48f91b186bf74599c9ce417e6bed222
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-29 09:51:12 +0800

    commit

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -21,16 +21,10 @@ const Notification = () => {
       </a>
       <div className="dropdown-menu dropdown-menu-lg dropdown-menu-center mt-2 py-0">
         <div className="list-group list-group-flush">
-          <a
-            href=" "
-            className="text-center text-primary fw-bold border-bottom border-light py-3"
-          >
+          <div className="text-center text-primary fw-bold border-bottom border-light py-3">
             Notifications
-          </a>
-          <a
-            href="../pages/single-message.html"
-            className="list-group-item list-group-item-action border-bottom"
-          >
+          </div>
+          <div className="list-group-item list-group-item-action border-bottom">
             <div className="row align-items-center">
               <div className="col ps-0 ms-2">
                 <div className="d-flex justify-content-between align-items-center">
@@ -38,34 +32,32 @@ const Notification = () => {
                     <h4 className="h6 mb-0 text-small">Administrator</h4>
                   </div>
                   <div className="text-end">
-                    <small>2 hrs ago</small>
+                    <button className="close">
+                      <i className="lni lni-close"></i>
+                    </button>
                   </div>
                 </div>
+                <small>2 hrs ago</small>
                 <p className="font-small mt-1 mb-0">
                   New message: "We need to improve the UI/UX for the landing
                   page."
                 </p>
               </div>
             </div>
-          </a>
+          </div>
           <a
             href=" "
             className="dropdown-item text-center fw-bold rounded-bottom py-3"
           >
             <svg
-              className="icon icon-xxs text-gray-400 me-1"
+              className="icon icon-xxs me-1 text-second"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
-              <path
-                fillRule="evenodd"
-                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                clipRule="evenodd"
-              ></path>
+              <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
             </svg>
-            View all
+            Delete all
           </a>
         </div>
       </div>
