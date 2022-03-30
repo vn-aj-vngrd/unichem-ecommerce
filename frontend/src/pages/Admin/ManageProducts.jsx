c08a1672b9fda670516a47a8bc4641e5a35819f8
Barr1ga <74694397+Barr1ga@users.noreply.github.com>
2022-03-30 13:44:22 +0800

    fixed conflict on manage promo/products

--- frontend/src/pages/Admin/ManageProducts.jsx
+++ frontend/src/pages/Admin/ManageProducts.jsx
@@ -36,7 +36,6 @@ const ManageProducts = () => {
       <img
         className="avatar  border-gray-100"
         alt="img"
-<<<<<<< Updated upstream
         src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-1/265045880_4664465363610412_3077703784126999012_n.jpg?stp=dst-jpg_p100x100&_nc_cat=105&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeG6OCYCZ3CHvDCnn3gkw4f07VEmKdmD4HrtUSYp2YPgeijaAgkLlVvIZi6OkafPGr9I9Ew1JO3LnF6iAYvJVZH-&_nc_ohc=R_Ut2OKr7rYAX_fWA-c&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fceb6-1.fna&oh=00_AT_3yf_OfUe6nfYGrjiXaBTCciNNCn3FpGZJl_pgLL2cAQ&oe=6246A9C3"
       />,
 
@@ -47,98 +46,6 @@ const ManageProducts = () => {
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       "Large",
       "Very Good",
-=======
-        src="https://cdn11.bigcommerce.com/s-03842/images/stencil/original/products/48860/67692/270-50ml__34267.1606845475.png?c=2"
-      />,
-      "1",
-      "Name",
-      "Brand",
-      "Category",
-      "Specifications",
-      "Types",
-      "Description",
-      "Quantities",
-      "Prices",
-      "SalePrices",
-      "Promo Status",
-      "Featured Status",
-      "Updated at",
-      "Created at",
-      <UpdateProduct />
-    ],
-    [
-      <img
-        className="avatar  border-gray-100"
-        alt="img"
-        src="https://cdn11.bigcommerce.com/s-03842/images/stencil/original/products/48860/67692/270-50ml__34267.1606845475.png?c=2"
-      />,
-      "1",
-      "Name",
-      "Brand",
-      "Category",
-      "Specifications",
-      "Types",
-      "Description",
-      "Quantities",
-      "Prices",
-      "SalePrices",
-      "Promo Status",
-      "Featured Status",
-      "Updated at",
-      "Created at",
-      <UpdateProduct />
-    ],
-    [
-      <img
-        className="avatar  border-gray-100"
-        alt="img"
-        src="https://cdn11.bigcommerce.com/s-03842/images/stencil/original/products/48860/67692/270-50ml__34267.1606845475.png?c=2"
-      />,
-      "1",
-      "Name",
-      "Brand",
-      "Category",
-      "Specifications",
-      "Types",
-      "Description",
-      "Quantities",
-      "Prices",
-      "SalePrices",
-      "Promo Status",
-      "Featured Status",
-      "Updated at",
-      "Created at",
-      <UpdateProduct />
-    ],
-    [
-      <img
-        className="avatar  border-gray-100"
-        alt="img"
-        src="https://cdn11.bigcommerce.com/s-03842/images/stencil/original/products/48860/67692/270-50ml__34267.1606845475.png?c=2"
-      />,
-      "1",
-      "Name",
-      "Brand",
-      "Category",
-      "Specifications",
-      "Types",
-      "Description",
-      "Quantities",
-      "Prices",
-      "SalePrices",
-      "Promo Status",
-      "Featured Status",
-      "Updated at",
-      "Created at",
-      <UpdateProduct />
-    ],
-    [
-      <img
-        className="avatar  border-gray-100"
-        alt="img"
-        src="https://cdn11.bigcommerce.com/s-03842/images/stencil/original/products/48860/67692/270-50ml__34267.1606845475.png?c=2"
-      />,
->>>>>>> Stashed changes
       "1",
       "299.99",
       "None",
@@ -152,7 +59,6 @@ const ManageProducts = () => {
       <img
         className="avatar  border-gray-100"
         alt="img"
-<<<<<<< Updated upstream
         src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-1/265045880_4664465363610412_3077703784126999012_n.jpg?stp=dst-jpg_p100x100&_nc_cat=105&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeG6OCYCZ3CHvDCnn3gkw4f07VEmKdmD4HrtUSYp2YPgeijaAgkLlVvIZi6OkafPGr9I9Ew1JO3LnF6iAYvJVZH-&_nc_ohc=R_Ut2OKr7rYAX_fWA-c&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fceb6-1.fna&oh=00_AT_3yf_OfUe6nfYGrjiXaBTCciNNCn3FpGZJl_pgLL2cAQ&oe=6246A9C3"
       />,
 
@@ -163,10 +69,6 @@ const ManageProducts = () => {
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       "Large",
       "Very Good",
-=======
-        src="https://cdn11.bigcommerce.com/s-03842/images/stencil/original/products/48860/67692/270-50ml__34267.1606845475.png?c=2"
-      />,
->>>>>>> Stashed changes
       "1",
       "299.99",
       "None",
--- frontend/src/pages/Admin/ManagePromotions.jsx
+++ frontend/src/pages/Admin/ManagePromotions.jsx
@@ -7,18 +7,13 @@ import CreatePromotion from "../../components/CreatePromotion";
 import UpdatePromotion from "../../components/UpdatePromo";
 
 const ManagePromotions = () => {
-<<<<<<< Updated upstream
   const columns = ["Promo Image", "Promo ID", "Promo Name", "Updated", "Created", ""];
-=======
-  const columns = ["Image", "Promotion ID", "Name", "Source", "Updated", "Created", ""];
->>>>>>> Stashed changes
 
   const data = [
     [
       <img
         className="avatar border-gray-100"
         alt="img"
-<<<<<<< Updated upstream
         src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-1/265045880_4664465363610412_3077703784126999012_n.jpg?stp=dst-jpg_p100x100&_nc_cat=105&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeG6OCYCZ3CHvDCnn3gkw4f07VEmKdmD4HrtUSYp2YPgeijaAgkLlVvIZi6OkafPGr9I9Ew1JO3LnF6iAYvJVZH-&_nc_ohc=R_Ut2OKr7rYAX_fWA-c&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fceb6-1.fna&oh=00_AT_3yf_OfUe6nfYGrjiXaBTCciNNCn3FpGZJl_pgLL2cAQ&oe=6246A9C3"
       />,
       "622c063496e12c68961c34ac",
@@ -28,46 +23,6 @@ const ManagePromotions = () => {
       <UpdatePromotion />,
     ],
   ];
-=======
-        src="https://cdn.mos.cms.futurecdn.net/V6LCHNxfSPT2Sxpr4bAzD.jpg"
-      />,
-      "Promotion ID", "Name", "Source", "Updated", "Created", <UpdatePromotion />
-    ],
-    [
-      <img
-        className="avatar border-gray-100"
-        alt="img"
-        src="https://cdn.mos.cms.futurecdn.net/V6LCHNxfSPT2Sxpr4bAzD.jpg"
-      />,
-      "Promotion ID", "Name", "Source", "Updated", "Created", <UpdatePromotion />
-    ],
-    [
-      <img
-        className="avatar border-gray-100"
-        alt="img"
-        src="https://cdn.mos.cms.futurecdn.net/V6LCHNxfSPT2Sxpr4bAzD.jpg"
-      />,
-      "Promotion ID", "Name", "Source", "Updated", "Created", <UpdatePromotion />
-    ],
-    [
-      <img
-        className="avatar border-gray-100"
-        alt="img"
-        src="https://cdn.mos.cms.futurecdn.net/V6LCHNxfSPT2Sxpr4bAzD.jpg"
-      />,
-      "Promotion ID", "Name", "Source", "Updated", "Created", <UpdatePromotion />
-    ],
-    [
-      <img
-        className="avatar border-gray-100"
-        alt="img"
-        src="https://cdn.mos.cms.futurecdn.net/V6LCHNxfSPT2Sxpr4bAzD.jpg"
-      />,
-      "Promotion ID", "Name", "Source", "Updated", "Created", <UpdatePromotion />
-    ],
-
-  ]
->>>>>>> Stashed changes
   useEffect(() => {
     document.title = "Unichem Store | Promotions";
   });
