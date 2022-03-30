98b1fb4948002637812f21eb4498025e207967be
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-03-30 21:03:17 +0800

    commit

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -16,68 +16,116 @@ const getCarts = asyncHandler(async (req, res) => {
 // @route   POST /api/Carts
 // @access  Private
 const setCart = asyncHandler(async (req, res) => {
-  let cart;
-  const existingCart = await Cart.findOne();
+  const existingCart = await Cart.findOne({
+    productID: req.body.productID,
+    productType: req.body.productType,
+  });
 
+  // If cart does not exist then create.
   if (!existingCart) {
-    cart = await Cart.create({
+    const newCart = await Cart.create({
       userID: req.user.id,
       productID: req.body.productID,
       productType: req.body.productType,
       quantity: req.body.quantity,
     });
-    return res.status(200).json(cart);
+    res.status(200).json(newCart);
   }
 
-  if (existingCart.quantity + req.body.quantity <= req.body.max) {
-    const updateCart = await Cart.findOneAndUpdate({
+  // If cart "current quantity + req quanity" is greater than max then throw error
+  if (existingCart.quantity + req.body.quantity > req.body.max) {
+    res.status(400);
+    throw new Error("Product quantity exceeds maximum quantity");
+  }
+
+  // If cart exists then update quantity
+  const updatedCart = await Cart.findOneAndUpdate(
+    {
       productID: req.body.productID,
       productType: req.body.productType,
-      quantity: existingCart.quantity + req.body.quantity,
-    });
-    return res.status(200).json(updateCart);
-  }
+    },
+    { quantity: existingCart.quantity + req.body.quantity }
+  );
 
-  res.status(401);
-  throw new Error("Product quantity exceeds maximum quantity");
+  res.status(200).json(updatedCart);
 });
 
 // @desc    Update Cart
 // @route   PUT /api/Carts/:id
 // @access  Private
 const updateCart = asyncHandler(async (req, res) => {
-  // const Cart = await Cart.findById(req.params.id)
-  // if(!Cart) {
-  //   res.status(400);
-  //   throw new Error("Cart not found");
-  // }
-  // const user = await Cart.findById(req.user.id);
-  // // Check for user
-  // if (!user) {
-  //   res.status(401);
-  //   throw new Error("User not found");
-  // }
-  // // Make sure the logged in user matches the cart user
-  // if (Cart.userID.toString() !== user.id) {
-  //   res.status(401)
-  //   throw new Error("User not authorized");
-  // }
-  // const updateCart = await Cart.findByIdAndUpdate(
-  //   req.params.id,
-  //   req.body, {
-  //     new: true,
-  //   }
-  // )
-  // res.status(200).json(updateCart);
+  const existingCart = await Cart.findById({
+    productID: req.body.productID,
+    productType: req.body.productType,
+  });
+
+  // Check for cart
+  if (!existingCart) {
+    res.status(400);
+    throw new Error("Cart not found");
+  }
+
+  // Check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the cart user
+  if (existingCart.userID.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  // Update cart
+  const updatedCart = await Cart.findOneAndUpdate(
+    {
+      productID: req.body.productID,
+      productType: req.body.productType,
+    },
+    { quantity: req.body.quantity }
+  );
+
+  res.status(200).json(updatedCart);
 });
 
 // @desc    Delete Cart
 // @route   DELETE /api/Carts/:id
 // @access  Private
-const deleteCart = asyncHandler(async (req, res) => {});
+const deleteCart = asyncHandler(async (req, res) => {
+  const existingCart = await Cart.findById({
+    productID: req.body.productID,
+    productType: req.body.productType,
+  });
+
+  // Check for cart
+  if (!existingCart) {
+    res.status(400);
+    throw new Error("Cart not found");
+  }
+
+  // Check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the cart user
+  if (existingCart.userID.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  await existingCart.remove();
+
+  res
+    .status(200)
+    .json({ productID: req.body.productID, productType: req.body.productType });
+});
 
 module.exports = {
   getCarts,
   setCart,
+  updateCart,
   deleteCart,
 };
