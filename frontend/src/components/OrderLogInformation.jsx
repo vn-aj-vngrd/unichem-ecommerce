b486c617e85cb4a3f5e5f7ce900225d4708297b5
Barr1ga <74694397+Barr1ga@users.noreply.github.com>
2022-05-11 20:10:04 +0800

    order log add on

--- frontend/src/components/OrderLogInformation.jsx
+++ frontend/src/components/OrderLogInformation.jsx
@@ -47,7 +47,7 @@ const OrderLogInformation = () => {
     );
   }
 
-  console.log(orders)
+  console.log(orders);
 
   return (
     <>
@@ -56,7 +56,13 @@ const OrderLogInformation = () => {
           <div className="product-grid">
             <div className="order-status">
               <div className="order-status-progress">
-                <div className={"order-status-progress-bar " + "progress-" + orders[0].orderStatus.toLowerCase().replaceAll(" ", "-")}></div>
+                <div
+                  className={
+                    "order-status-progress-bar " +
+                    "progress-" +
+                    orders[0].orderStatus.toLowerCase().replaceAll(" ", "-")
+                  }
+                ></div>
               </div>
               <div className="one-status">
                 <button
@@ -114,13 +120,13 @@ const OrderLogInformation = () => {
 
           <div className="order-status-log-details">
             {orders[0].statusDates &&
-              orders[0].statusDates.map((log, index) => (
+              orders[0].statusDates.slice(0, 6).map((log, index) => (
                 <div key={index} className="order-status-single-log">
                   <div className="order-details-date">
                     {log.date ? moment(log.date).format("DD/MM/YY") : ""}
                   </div>
                   <div className="order-details-status-progress">
-                    {index < orders[0].statusDates.length - 1 && (
+                    {index < orders[0].statusDates.slice(0, 6).length - 1 && (
                       <div className="vertical-bar">
                         <div
                           className={
@@ -148,6 +154,108 @@ const OrderLogInformation = () => {
                   </div>
                 </div>
               ))}
+
+            <hr></hr>
+            <br></br>
+            {orders[0].statusDates[7] && (
+              <div className="order-status-single-log">
+                <div className="order-details-date">
+                  {orders[0].statusDates[7].date
+                    ? moment(orders[0].statusDates[7].date).format("DD/MM/YY")
+                    : ""}
+                </div>
+                <div className="order-details-status-progress">
+                  <div
+                    className={
+                      orders[0].statusDates[7].date
+                        ? "status-circle-log-checked"
+                        : "status-circle-log-unchecked"
+                    }
+                  >
+                    <i className="lni lni-checkmark"></i>
+                  </div>
+                </div>
+                <div className="order-details-description">
+                  <h6>{orders[0].statusDates[7].label}</h6>
+                  <p>{orders[0].statusDates[7].desc}</p>
+                </div>
+              </div>
+            )}
+
+            {orders[0].statusDates[8] && (
+              <div className="order-status-single-log">
+                <div className="order-details-date">
+                  {orders[0].statusDates[8].date
+                    ? moment(orders[0].statusDates[8].date).format("DD/MM/YY")
+                    : ""}
+                </div>
+                <div className="order-details-status-progress">
+                  <div
+                    className={
+                      orders[0].statusDates[8].date
+                        ? "status-circle-log-checked"
+                        : "status-circle-log-unchecked"
+                    }
+                  >
+                    <i className="lni lni-checkmark"></i>
+                  </div>
+                </div>
+                <div className="order-details-description">
+                  <h6>{orders[0].statusDates[8].label}</h6>
+                  <p>{orders[0].statusDates[8].desc}</p>
+                </div>
+              </div>
+            )}
+
+            {orders[0].statusDates[9] && (
+              <div className="order-status-single-log">
+                <div className="order-details-date">
+                  {orders[0].statusDates[9].date
+                    ? moment(orders[0].statusDates[9].date).format("DD/MM/YY")
+                    : ""}
+                </div>
+                <div className="order-details-status-progress">
+                  <div
+                    className={
+                      orders[0].statusDates[9].date
+                        ? "status-circle-log-checked"
+                        : "status-circle-log-unchecked"
+                    }
+                  >
+                    <i className="lni lni-checkmark"></i>
+                  </div>
+                </div>
+                <div className="order-details-description">
+                  <h6>{orders[0].statusDates[9].label}</h6>
+                  <p>{orders[0].statusDates[9].desc}</p>
+                </div>
+              </div>
+            )}
+
+            {orders[0].statusDates[10] && (
+              <div className="order-status-single-log">
+                <div className="order-details-date">
+                  {orders[0].statusDates[10].date
+                    ? moment(orders[0].statusDates[10].date).format("DD/MM/YY")
+                    : ""}
+                </div>
+                <div className="order-details-status-progress">
+                  <div
+                    className={
+                      orders[0].statusDates[10].date
+                        ? "status-circle-log-checked"
+                        : "status-circle-log-unchecked"
+                    }
+                  >
+                    <i className="lni lni-checkmark"></i>
+                  </div>
+                </div>
+                <div className="order-details-description">
+                  <h6>{orders[0].statusDates[10].label}</h6>
+                  <p>{orders[0].statusDates[10].desc}</p>
+                </div>
+              </div>
+            )}
           </div>
 
           <div className="product">
