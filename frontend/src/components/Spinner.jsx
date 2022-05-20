a6a887aac4af93d7209618f2d80729b5495c8c8f
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-05-20 21:05:24 +0800

    change spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import { css } from "@emotion/react";
+import BeatLoader from "react-spinners/BeatLoader";
+import PuffLoader from "react-spinners/PuffLoader";
+
+// Can be a string as well. Need to ensure each key-value pair ends with ;
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: #f44336;
+`;
 
 const Spinner = ({ globalSpinner }) => {
   if (globalSpinner !== undefined && globalSpinner === "true") {
     return (
       <div className="loading">
-        <div className="loading-spinner"></div>
+        <PuffLoader color="#f44336" loading={true} css={override} size={60} />
+        {/* <div className="loading-spinner"></div> */}
       </div>
     );
   }
+
   return (
     <div className="spinner-container">
-      <div className="loading-spinner"></div>
+      <BeatLoader color="#f44336" loading={true} css={override} size={13} title="Loading..." />
+      
     </div>
   );
 };
