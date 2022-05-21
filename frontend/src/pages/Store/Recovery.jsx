61575a9e9bcd09e7d1fc6008dfbf8017fa639df4
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-05-21 23:23:32 +0800

    fixes

--- frontend/src/pages/Store/Recovery.jsx
+++ frontend/src/pages/Store/Recovery.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
 import {
   validateRecovery,
   recoverAccount,
-  resetUser,
 } from "../../features/auth/authSlice";
 import PageNotFound from "../PageNotFound";
 import Spinner from "../../components/Spinner";
@@ -13,36 +13,25 @@ import Swal from "sweetalert2";
 const Verification = () => {
   const [call, setCall] = useState(false);
   const [validUrl, setValidUrl] = useState(false);
-  const [error, setError] = useState();
 
-  const [formData, setFormData] = useState({
-    password1: "",
-    password2: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const { password1, password2 } = formData;
+  const password1 = watch("password1");
 
   const param = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { user, isLoading, isSuccess, isAccountRecovered } = useSelector(
-    (state) => state.auth
-  );
+  const { isLoading, isSuccess, message, isAccountRecovered, isError } =
+    useSelector((state) => state.auth);
 
   useEffect(() => {
     document.title = "Unichem Store | Account Recovery";
-
-    if (user) {
-      navigate("/");
-    }
 
     if (!call) {
       dispatch(validateRecovery(param));
@@ -51,6 +40,16 @@ const Verification = () => {
 
     if (isSuccess) {
       setValidUrl(true);
+    }
+
+    if (isError) {
+      Swal.fire({
+        title: "Something went wrong!",
+        text: message,
+        icon: "error",
+        confirmButtonColor: "#f44336",
+      });
+      navigate("/login");
     }
 
     if (isAccountRecovered) {
@@ -62,93 +61,108 @@ const Verification = () => {
       });
       navigate("/login");
     }
+  }, [
+    param,
+    isSuccess,
+    isError,
+    isAccountRecovered,
+    call,
+    message,
+    navigate,
+    dispatch,
+  ]);
 
-    return () => {
-      dispatch(resetUser());
-    };
-  }, [user, param, isSuccess, isAccountRecovered, call, navigate, dispatch]);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    setError();
-
-    if (formData.password1.length < 8) {
-      setError("Password must be at least 8 characters");
-      return;
-    }
-
-    if (formData.password1 !== formData.password2) {
-      setError("Password does not match");
-      return;
-    }
-
+  const onSubmit = (data) => {
     const recoveryData = {
       param,
-      password: password1,
+      password: data.password1,
     };
 
+    // console.log(recoveryData);
     dispatch(recoverAccount(recoveryData));
   };
+
+  if (isLoading) {
+    return <Spinner globalSpinner="true" />;
+  }
 
   return (
     <>
       {validUrl ? (
         <div className="account-login">
-          {isLoading ? (
-            <div className="container">
-              <Spinner />
+          <div className="container">
+            <div className="section-title">
+              <h2>Recover Account</h2>
+              <p>Please input your new password below.</p>
             </div>
-          ) : (
-            <div className="container">
-              <div className="section-title">
-                <h2>Recover Account</h2>
-                <p>Please input your new password below.</p>
-              </div>
-              <div className="row">
-                <div className="col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-12">
-                  <form className="card login-form" onSubmit={onSubmit}>
-                    <div className="card-body">
-                      <div className="form-group input-group">
-                        <label>New Password</label>
-                        <input
-                          type="password"
-                          className="form-control"
-                          id="password1"
-                          name="password1"
-                          value={password1}
-                          onChange={onChange}
-                          required
-                        />
-                      </div>
-
-                      <div className="form-group input-group">
-                        <label>Confirm New Password</label>
-                        <input
-                          type="password"
-                          className="form-control"
-                          id="password2"
-                          name="password2"
-                          value={password2}
-                          onChange={onChange}
-                          required
-                        />
-                      </div>
-
-                      <div className="button">
-                        <button className="btn" type="submit">
-                          Send
-                        </button>
-                      </div>
-                      <div className="text-center mt-4">
-                        {error && <small className="text-red">{error}</small>}
-                      </div>
+            <div className="row">
+              <div className="col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-12">
+                <form
+                  className="card login-form"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <div className="card-body">
+                    <div className="form-group input-group">
+                      <label>New Password</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        {...register("password1", {
+                          required: {
+                            value: true,
+                            message: "Password is required",
+                          },
+                          minLength: {
+                            value: 8,
+                            message: "Password must be at least 8 characters",
+                          },
+                        })}
+                        style={{
+                          border: errors.password1 ? "1px solid #f44336" : "",
+                        }}
+                      />
+                      {errors.password1 && (
+                        <p className="error-message">
+                          ⚠ {errors.password1.message}
+                        </p>
+                      )}
                     </div>
-                  </form>
-                </div>
+
+                    <div className="form-group input-group">
+                      <label>Confirm New Password</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        {...register("password2", {
+                          required: {
+                            value: true,
+                            message: "Confirm Password is required",
+                          },
+                          validate: (value) =>
+                            password1 === value || "Passwords do not match",
+                        })}
+                        style={{
+                          border: errors.password2 ? "1px solid #f44336" : "",
+                        }}
+                      />
+                      {errors.password2 && (
+                        <p className="error-message">
+                          ⚠ {errors.password2.message}
+                        </p>
+                      )}
+                    </div>
+
+                    <div className="button">
+                      <button className="btn" type="submit">
+                        Send
+                      </button>
+                    </div>
+                    <div className="text-center mt-4"></div>
+                  </div>
+                </form>
               </div>
             </div>
-          )}
+          </div>
         </div>
       ) : (
         <>
--- frontend/src/pages/Store/Verification.jsx
+++ frontend/src/pages/Store/Verification.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { verifyUser, resetUser } from "../../features/auth/authSlice";
+import { verifyUser } from "../../features/auth/authSlice";
 import PageNotFound from "../PageNotFound";
 import Spinner from "../../components/Spinner";
 
@@ -13,61 +13,52 @@ const Verification = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { user, isLoading, isSuccess } = useSelector((state) => state.auth);
+  const { isLoading, isSuccess } = useSelector((state) => state.auth);
 
   useEffect(() => {
     document.title = "Unichem Store | Email Verification";
 
-    if (user) {
-      navigate("/");
-    }
-
     if (!call) {
       dispatch(verifyUser(param));
-
       setCall(true);
     }
 
     if (isSuccess) {
       setValidUrl(true);
     }
+  }, [param, isSuccess, call, navigate, dispatch]);
 
-    return () => {
-      dispatch(resetUser());
-    };
-  }, [user, param, isSuccess, call, navigate, dispatch]);
+  if (isLoading) {
+    return <Spinner globalSpinner="true" />;
+  }
 
   return (
     <>
       {validUrl ? (
         <div className="content">
-          {isLoading ? (
-            <Spinner />
-          ) : (
-            <div className="success-area">
-              <div className="d-table">
-                <div className="d-table-cell">
-                  <div className="container">
-                    <div className="success-content">
-                      <h1>
-                        <i className="lni lni-checkmark-circle"></i>
-                      </h1>
-                      <h2>Email Verified</h2>
-                      <p>
-                        Your email address has been verified, and you can now
-                        access your account.
-                      </p>
-                      <div className="button">
-                        <Link to="/login" className="btn">
-                          Log in
-                        </Link>
-                      </div>
+          <div className="success-area">
+            <div className="d-table">
+              <div className="d-table-cell">
+                <div className="container">
+                  <div className="success-content">
+                    <h1>
+                      <i className="lni lni-checkmark-circle"></i>
+                    </h1>
+                    <h2>Email Verified</h2>
+                    <p>
+                      Your email address has been verified, and you can now
+                      access your account.
+                    </p>
+                    <div className="button">
+                      <Link to="/login" className="btn">
+                        Log in
+                      </Link>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          )}
+          </div>
         </div>
       ) : (
         <>
